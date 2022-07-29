d861224efcaece4fc4853d3c98b411d2bb04bf3d
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-07-29 10:15:31 -0500

    Initial Commit

---
+++ something-borrowed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  something-borrowed
+//
+//  Created by Tiffini Hyatt on 7/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ something-borrowed/something_borrowedApp.swift
@@ -0,0 +1,17 @@
+//
+//  something_borrowedApp.swift
+//  something-borrowed
+//
+//  Created by Tiffini Hyatt on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct something_borrowedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
