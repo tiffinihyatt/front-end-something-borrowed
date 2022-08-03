c91f7f459ba95f4c6162851bba068944766d8c86
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-08-02 21:38:42 -0500

    Add placeholder browse view

---
+++ something-borrowed/Views/BrowseView.swift
@@ -0,0 +1,27 @@
+//
+//  BrowseView.swift
+//  something-borrowed
+//
+//  Created by Tiffini Hyatt on 8/2/22.
+//
+
+import SwiftUI
+
+struct BrowseView: View {
+//    create new property to hold list of garments from API request
+//    change List View to convert each garment into a specific UI
+    
+    var body: some View {
+        List {
+            Text("Garment")
+            Text("Garment")
+            Text("Garment")
+        }
+    }
+}
+
+struct BrowseView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseView()
+    }
+}
