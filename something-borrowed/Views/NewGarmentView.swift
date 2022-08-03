154e4d7afd784c16a417359da7e02e5521aecbd4
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-08-02 22:21:13 -0500

    Add user input text fields to NewGarmentView

--- something-borrowed/Views/NewGarmentView.swift
+++ something-borrowed/Views/NewGarmentView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct NewGarmentView: View {
+    @State private var title: String = ""
+    @State private var brand: String = ""
+//    street size
+//    label size
+    @State private var color: String = ""
+    @State private var condition: String = ""
+//    price
+    @State private var description: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            List {
+                TextField("Item Name", text: $title)
+                TextField("Brand", text: $brand)
+                TextField("Color", text: $color)
+                TextField("Condition", text: $condition)
+                TextField("Description", text: $description)
+            }
+        }
     }
 }
 
