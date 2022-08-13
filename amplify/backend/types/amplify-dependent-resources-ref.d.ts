5366c251d4723ef58f8bfc48147ef0261e8d71c1
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-08-13 11:51:57 -0500

    Add configuration for amplify/S3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "somethingborroweda8c77e78": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s320e561f9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- something-borrowed/something_borrowedApp.swift
+++ something-borrowed/something_borrowedApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSCognitoAuthPlugin
+import AWSS3StoragePlugin
 
 @main
 struct something_borrowedApp: App {
+    
+    init() {
+            configureAmplify()
+        }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+    private func configureAmplify() {
+            do {
+                try Amplify.add(plugin: AWSCognitoAuthPlugin())
+                try Amplify.add(plugin: AWSS3StoragePlugin())
+                
+                try Amplify.configure()
+                print("Successfully configured Amplify")
+                
+            } catch {
+                print("Could not configure Amplify", error)
+            }
+        }
 }
